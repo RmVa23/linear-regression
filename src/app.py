44e10786adf13cc8b744947ff38637a45b7d4dc5
RmVa23 <148461819+RmVa23@users.noreply.github.com>
2024-03-13 19:40:34 +0000

    instalacion paquetes, creacion y conexion BD

--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
+import pandas as pd
 from utils import db_connect
 engine = db_connect()
 
 # your code here
+engine
+if engine:
+    print("Conexión exitosa")
+
+data = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/linear-regression-project-tutorial/main/medical_insurance_cost.csv')
+data.to_sql('tabla seguro', engine, if_exists='replace', index=False)
